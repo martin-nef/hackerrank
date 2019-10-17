42a031850f873655e3d31535cf96aa6e10913db4
Martin Nefjodov <Martin.Nefjodov@agileautomations.co.uk>
2019-10-17 19:38:52 +0100

    Started Jumping on clouds challenge

---
+++ InterviewChallenges/JumpingOnTheClouds/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace JumpingOnTheClouds {
+    class Program {
+
+        // Complete the jumpingOnClouds function below.
+        static int jumpingOnClouds (int[] c) {
+            throw new NotImplementedException();
+        }
+
+        static void Main (string[] args) {
+            var input = new [] { 0, 0, 0, 0, 1, 0 };
+            Console.WriteLine (jumpingOnClouds (input));
+        }
+    }
+}
---
+++ InterviewChallenges/JumpingOnTheClouds/readme.md
@@ -0,0 +1,2 @@
+Jumping on Clouds
+[link](https://www.hackerrank.com/challenges/jumping-on-the-clouds)
