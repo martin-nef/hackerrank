40e4ce101c32e9e374f0876f9b05700ddf8b688b
Martin Nefjodov <Martin.Nefjodov@agileautomations.co.uk>
2019-10-17 20:12:12 +0100

    Started Repeated String

---
+++ InterviewChallenges/RepeatedString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RepeatedString {
+    class Program {
+        // Complete the repeatedString function below.
+        static long repeatedString (string s, long n) {
+            throw new NotImplementedException();
+        }
+
+        static void Main (string[] args) {
+            Console.WriteLine ($"ans: {7}  res:{repeatedString ("aba", 10)}");
+        }
+    }
+}
