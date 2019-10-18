38447c30301dfec876b82641a835fa3fb0978cc4
Martin Nefjodov <Martin.Nefjodov@agileautomations.co.uk>
2019-10-18 01:23:35 +0100

    .

--- InterviewChallenges/RepeatedString/Program.cs
+++ InterviewChallenges/RepeatedString/Program.cs
@@ -16,11 +16,40 @@ namespace RepeatedString {
     class Program {
         // Complete the repeatedString function below.
         static long repeatedString (string s, long n) {
-            throw new NotImplementedException();
+            char countedChar = 'a';
+
+            // Unexpected input
+            if (string.IsNullOrEmpty (s) || s.Length > 100) {
+                throw new ArgumentException (nameof (s), "Need |s| >= 1");
+            }
+            if (n <= 0) {
+                throw new ArgumentException (nameof (s), "Need n >= 1");
+            }
+
+            // O(|s|)
+            int asInString = s.Count (x => x == countedChar);
+            if (asInString == 0) {
+                return 0;
+            }
+
+            // Find how many times all of s fits in the n-long string
+            long fullRepeats = (long) Math.Floor ((double) n / s.Length); // (cast n to double, otherwise result will be cast to long and rounded)
+            // Find how long the rest of the n-long string is
+            long lenOfTrailingPart = n % (long) s.Length;
+
+            // Count a's in the whole part of the string
+            return fullRepeats * asInString +
+                // Count a's in incomplete part of s at the end of the n-long string
+                s.Take ((int) lenOfTrailingPart) // (fast, since lenOfTrailingPart < |s| <= 100)
+                .Count (x => x == countedChar);
         }
 
         static void Main (string[] args) {
-            Console.WriteLine ($"ans: {7}  res:{repeatedString ("aba", 10)}");
+            var res = repeatedString ("aba", 10);
+            var res2 = repeatedString ("abcac", 10);
+            Console.WriteLine ($"ans: {7}  res:{res}");
+            Console.WriteLine ($"ans: {4}  res:{res2}");
+            Console.WriteLine ($"ans: ?  res:{repeatedString ("a", (long)Math.Pow(10, 12))}");
         }
     }
 }
