fcab016c520b96349749b72f70abea7b1ca1aa22
Martin Nefjodov <Martin.Nefjodov@agileautomations.co.uk>
2019-10-23 01:21:44 +0100

    Partial solution to New Year Chaos

--- InterviewChallenges/NewYearChaos/Program.cs
+++ InterviewChallenges/NewYearChaos/Program.cs
@@ -16,70 +16,82 @@ using System.Threading.Tasks;
 using Xunit;
 
 namespace NewYearChaos {
-    public class Program {
-        static readonly TextWriter console = new StringWriter ();
-        static void Log (string message) {
-            console.WriteLine (message);
-        }
+    public static class Program {
+        public static string minimumBribes (int[] q) {
+            var output = default (string);
+            var maximumBribe = 2;
+            var totalDistanceMoved = 0;
 
-        // Complete the minimumBribes function below.
-        static void minimumBribes (int[] q) {
-            var isValidQ = true;
-            if (!isValidQ) {
-                Log ("Too chaotic");
-                return;
-            }
+            // Calculate, how far each person has moved.
+            for (int i = 0; i < q.Length; i++) {
+                var originalPosition = q[i];
+                var currentPosition = i + 1;
 
-            var minBribes = 0;
-
-            // TODO: calculate minBribes
-
-            Log ($"{minBribes}");
-        }
-
-        /// <summary>
-        /// Minimum bribes for the q to go from "1 2 3 4 5" to a given state.
-        /// Each person can only bribe once
-        /// </summary>
-        [Fact]
-        public void TestMinimumBribes () {
-            var test1 = 
-                        "2 5 1 3 4 - Too chaotic" +
-                        "\n" + 
-                        "2 1 5 3 4 - 3"
-                        ;
-            var testNumber = 1;
-            foreach (var test in StringToTestData (test1)) {
-                minimumBribes (test.Input);
-                string output = console.ToString ().Split ("\n", StringSplitOptions.RemoveEmptyEntries).Last ().Trim ();
-                if (test.Expected != output) {
-                    throw new Exception ($"Test {testNumber++} failed:\nInput {string.Join (" ", test.Input)}\nExpected {test.Expected}\nOutput {output}\n");
+                // Calculate how many bribes it would take to move froward from original to current
+                var distanceMoved = originalPosition - currentPosition;
+                // A person can accept as many bribes as they like, so distance can be negative, yet not too chaotic
+                #error Calculating bribes from distance does not account for case when bribe is made after accepting any
+                if (distanceMoved > maximumBribe) {
+                    output = "Too chaotic";
+                    break;
+                }
+                if (distanceMoved > 0) {
+                    totalDistanceMoved += distanceMoved;
                 }
             }
+            if (output == null) {
+                output = $"{totalDistanceMoved}";
+            }
+            return output;
         }
 
-        static IEnumerable<TestData> StringToTestData (string numberString) {
-            return numberString.Split ('\n', StringSplitOptions.RemoveEmptyEntries)
-                .Select (x => x.Split ('-', StringSplitOptions.RemoveEmptyEntries)
-                    .Select (y => y.Trim ())
-                    .ToArray ())
-                .Select (x => new TestData { Input = ToArray<int> (x[0]), Expected = x[1] });
-        }
+        public class ProgramTest {
 
-        class TestData {
-            public int[] Input { get; set; }
-            public string Expected { get; set; }
-        }
-
-        static TNumber[] ToArray<TNumber> (string numbers) where TNumber : new () {
-            return numbers?.Split (new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
-                .Select (x => {
-                    if (!double.TryParse (x, out double y)) {
-                        throw new Exception ($"Couldn't parse '{x}' to number");
+            /// <summary>
+            /// Minimum bribes for the q to go from "1 2 3 4 5" to a given state.
+            /// Each person can only bribe once
+            /// </summary>
+            [Fact]
+            public void TestMinimumBribes () {
+                var test1 =
+                    "2 5 1 3 4 - Too chaotic\n" +
+                    "1 2 3 4 5 6 - 0\n" +
+                    "2 1 5 3 4 - 3\n" +
+                    "5 1 2 3 7 8 6 4 - Too chaotic\n" +
+                    "1 2 5 3 7 8 6 4 - 7\n";
+                var testNumber = 0;
+                foreach (var test in StringToTestData (test1)) {
+                    testNumber++;
+                    var output = Program.minimumBribes (test.Input);
+                    if (test.Expected != output) {
+                        throw new Exception ($"Test {testNumber} failed:\nInput {string.Join (" ", test.Input)}\nExpected {test.Expected}\nOutput {output}\n");
                     }
-                    return (TNumber) Convert.ChangeType (y, typeof (TNumber));
-                })
-                .ToArray ();
+                }
+            }
+
+            static IEnumerable<TestData> StringToTestData (string numberString) {
+                return numberString.Split ('\n', StringSplitOptions.RemoveEmptyEntries)
+                    .Select (x => x.Split ('-', StringSplitOptions.RemoveEmptyEntries)
+                        .Select (y => y.Trim ())
+                        .ToArray ())
+                    .Select (x => new TestData { Input = ToArray<int> (x[0]), Expected = x[1] });
+            }
+
+            class TestData {
+                public int[] Input { get; set; }
+                public string Expected { get; set; }
+            }
+
+            static TNumber[] ToArray<TNumber> (string numbers) where TNumber : new () {
+                return numbers?.Split (new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select (x => {
+                        if (!double.TryParse (x, out double y)) {
+                            throw new Exception ($"Couldn't parse '{x}' to number");
+                        }
+                        return (TNumber) Convert.ChangeType (y, typeof (TNumber));
+                    })
+                    .ToArray ();
+            }
         }
     }
 }
